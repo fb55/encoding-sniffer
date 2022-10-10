a522e0e7a881e997ccb5debf236966b1bce302a3
Felix Böhm <188768+fb55@users.noreply.github.com>
2022-10-10 14:12:43 +0100

    test: Add tests for edge-cases
    Both are currently failing

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -98,6 +98,28 @@ describe("Sniffer", () => {
         expect(sniffer.resultType).toBe(ResultType.META_TAG);
     });
 
+    it("should ignore duplicate meta content attributes", () => {
+        const sniffer = new Sniffer();
+        sniffer.write(
+            Buffer.from(
+                "<meta content=CHARSET='iso-8859-2' content=charset=UTF-16LE http-equiv='content-type'>"
+            )
+        );
+        expect(sniffer.encoding).toBe("ISO-8859-2");
+        expect(sniffer.resultType).toBe(ResultType.META_TAG);
+    });
+
+    it("should ignore the slash of self-closing meta tags", () => {
+        const sniffer = new Sniffer();
+        sniffer.write(
+            Buffer.from(
+                "<meta http-equiv='content-type' content=CHARSET=iso-8859-2/>"
+            )
+        );
+        expect(sniffer.encoding).toBe("ISO-8859-2");
+        expect(sniffer.resultType).toBe(ResultType.META_TAG);
+    });
+
     it("should support XML UTF-16LE prefixes", () => {
         const sniffer = new Sniffer();
         sniffer.write(STRINGS.UTF16LE_XML_PREFIX);
