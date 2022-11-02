14e8a299ecb28e9bff5468859fd3bf76e971d272
Felix <188768+fb55@users.noreply.github.com>
2022-11-02 13:58:56 +0100

    fix(stream): Don't decode strings (#308)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,7 +7,10 @@ function getStream(stream: NodeJS.ReadableStream): Promise<string> {
     // TODO[engines.node@>=18]: Use `reduce`
     return new Promise((resolve, reject) => {
         let data = "";
-        stream.on("data", (chunk) => (data += chunk));
+        stream.on("data", (chunk) => {
+            expect(typeof chunk).toBe("string");
+            data += chunk;
+        });
         stream.on("end", () => resolve(data));
         stream.on("error", reject);
         stream.resume();
